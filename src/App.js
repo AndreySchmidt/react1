b7e35d6457457748b0aecb9353cb84a400ff19ed
AndreySchmidt <ringnull@gmail.com>
2023-02-22 13:07:38 +0300

    html css

--- src/App.js
+++ src/App.js
@@ -6,13 +6,52 @@ function App() {
   return (
     <div className="app">
       <header>
-      header
+        <div className='header-wrapper'>
+          <div><img className='logo' src={logo} /></div>
+          <div><button className='btn-login'>login</button></div>
+        </div>
       </header>
-      <main>
-      main
-      </main>
+      <section>
+        <h1>CSS и HTML на практике</h1>
+
+        <div className='content-wrapper'>
+          <div className='card'>
+          <h2>Заголовок h2 первой карточки</h2>
+            <img className='card-img' src={logo} />
+            <p>
+              Some text Some text Some text Some text Some text Some text Some text Some text 
+              Some text Some text Some text Some text Some text Some text Some text
+            </p>
+          </div>
+
+          <div className='card'>
+          <h2>Заголовок h2</h2>
+            <img className='card-img' src={logo} />
+            <p>
+              Some text Some text Some text Some text Some text Some text Some text Some text 
+              Some text Some text Some text Some text Some textSome text Some text Some text Some text Some text Some text Some text Some
+               text Some textSome text Some text Some text Some text Some text Some text Some text
+            </p>
+          </div>
+
+          <div className='card'>
+          <h2>Заголовок h2</h2>
+            <img className='card-img' src={logo} />
+            <p>
+              Some text Some text Some text Some text Some text Some text Some text Some text 
+              Some text Some text Some text Some text Some text Some textSome text Some text Some text Some text Some text Some text Some text Some text
+            </p>
+          </div>
+        </div>
+
+      </section>
       <footer>
-      footer
+        <ul>
+          <li><a href=''>Ссылка</a></li>
+          <li><a href=''>Ещё одна</a></li>
+          <li><a href=''>Три</a></li>
+          <li><a href=''>Четыре</a></li>
+        </ul>
       </footer>
     </div>
   );
