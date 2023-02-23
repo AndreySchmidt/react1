0e027de85703749781d6772796ad1b4f795d6a4d
AndreySchmidt <ringnull@gmail.com>
2023-02-23 16:08:43 +0300

    вынос шаблона

--- src/App.js
+++ src/App.js
@@ -1,58 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import './scss/app.scss';
+// import './App.css';
+import { App1 } from './components/App1';
 
 function App() {
   return (
     <div className="app">
-      <header>
-        <div className='header-wrapper'>
-          <div><img className='logo' src={logo} /></div>
-          <div><button className='btn-login'>login</button></div>
-        </div>
-      </header>
-      <section>
-        <h1>CSS и HTML на практике</h1>
-
-        <div className='content-wrapper'>
-          <div className='card'>
-          <h2>Заголовок h2 первой карточки</h2>
-            <img className='card-img' src={logo} />
-            <p>
-              Some text Some text Some text Some text Some text Some text Some text Some text 
-              Some text Some text Some text Some text Some text Some text Some text
-            </p>
-          </div>
-
-          <div className='card'>
-          <h2>Заголовок h2</h2>
-            <img className='card-img' src={logo} />
-            <p>
-              Some text Some text Some text Some text Some text Some text Some text Some text 
-              Some text Some text Some text Some text Some textSome text Some text Some text Some text Some text Some text Some text Some
-               text Some textSome text Some text Some text Some text Some text Some text Some text
-            </p>
-          </div>
-
-          <div className='card'>
-          <h2>Заголовок h2</h2>
-            <img className='card-img' src={logo} />
-            <p>
-              Some text Some text Some text Some text Some text Some text Some text Some text 
-              Some text Some text Some text Some text Some text Some textSome text Some text Some text Some text Some text Some text Some text Some text
-            </p>
-          </div>
-        </div>
-
-      </section>
-      <footer>
-        <ul>
-          <li><a href=''>Ссылка</a></li>
-          <li><a href=''>Ещё одна</a></li>
-          <li><a href=''>Три</a></li>
-          <li><a href=''>Четыре</a></li>
-        </ul>
-      </footer>
+      <App1 />
     </div>
   );
 }
