2ba704d2ad92e10d5424d7916ddfac900abf8b59
AndreySchmidt <ringnull@gmail.com>
2023-02-26 23:14:32 +0300

    header

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -32,9 +32,87 @@ export const App2 = () => {
             </header>
 
             <main>
-                <section className=''>
+                <section className='section-main'>
                     <div className='container'>
-                        div
+                        <div className='sofa'>
+                            <div className='item'>
+                                <h1 className='title'>Modern Interior Design Studio</h1>
+                                <div className='buttons'>
+                                    <a className='btn btn-sofa'>Shop now</a>
+                                    <a className='btn btn-sofa outline'>Explore</a>
+                                </div>
+                            </div>
+                            <div className='img-sofa'><img src="/assets/img/sofa.png" /></div>
+                            <div className='dotted'>
+                                <svg width="260" height="176" viewBox="0 0 260 176" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <ellipse opacity="0.3" cx="4.5" cy="4" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="4" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="4" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="4" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="4" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="4" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="4" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="4" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="28" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="28" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="28" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="28" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="28" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="28" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="28" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="28" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="52" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="52" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="52" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="52" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="52" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="52" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="52" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="52" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="76" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="76" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="76" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="76" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="76" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="76" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="76" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="76" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="100" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="100" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="100" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="100" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="100" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="100" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="100" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="100" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="124" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="124" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="124" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="124" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="124" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="124" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="124" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="124" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="148" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="148" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="148" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="148" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="148" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="148" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="148" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="148" r="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="4.5" cy="172" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <ellipse opacity="0.3" cx="40.5" cy="172" rx="4.5" ry="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="76" cy="172" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="112" cy="172" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="148" cy="172" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="184" cy="172" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="220" cy="172" r="4" fill="#D9D9D9"/>
+                                    <circle opacity="0.3" cx="256" cy="172" r="4" fill="#D9D9D9"/>
+                                </svg>
+
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>
